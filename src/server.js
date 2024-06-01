5f9f4fec4dd732682fdc77890e4de8da71e02314
Atuokwu <atuokwu.fortune@gmail.com>
2024-06-01 22:20:09 +0100

    removed cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 import express from "express";
 import path from "path";
-// import cors from "cors";
 import router from "./routes";
 
 const app = express()
 app.use(express.json());
-// app.use(cors());
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.use('/', router);
